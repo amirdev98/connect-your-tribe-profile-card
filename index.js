fc309b91c9e022ca887f03c33da7cac34d6c341a
Amir Napoletano <amirnapoletano@gmail.com>
2023-02-14 21:01:14 +0100

    ✅ adde

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express from "express";
 
-const url = 'https://whois.fdnd.nl/api/v1/member/amir-amerian'
-const data = await fetch(url).then((response) => response.json())
+const url = "https://whois.fdnd.nl/api/v1/member/amir-amerian";
+//leg uit deze regel
+const data = await fetch(url).then((response) => response.json());
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 
 // Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   // res.send('Hello World!')
-  res.render('index', data)
-})
+  res.render("index", data);
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8023)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
